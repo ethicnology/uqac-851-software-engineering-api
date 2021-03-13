5d9ead274516fb6a3bd0d3476560123e98f533ce
ethicnology <ethicnology@pm.me>
2021-03-13 17:48:12 -0500

    Add routes for authentication and REST user

--- http/route/route.go
+++ http/route/route.go
@@ -1,32 +1,37 @@
 package route
 
 import (
-	"github.com/ethicnology/uqac-851-software-engineering-api/http/controller/hello"
+	"github.com/ethicnology/uqac-851-software-engineering-api/database/model"
+	"github.com/ethicnology/uqac-851-software-engineering-api/http/controller/user"
 
 	"goyave.dev/goyave/v3"
+	"goyave.dev/goyave/v3/auth"
 	"goyave.dev/goyave/v3/cors"
+	"goyave.dev/goyave/v3/middleware/ratelimiter"
 )
-
-// Routing is an essential part of any Goyave application.
-// Routes definition is the action of associating a URI, sometimes having
-// parameters, with a handler which will process the request and respond to it.
-
-// Routes are defined in routes registrer functions.
-// The main route registrer is passed to "goyave.Start()" and is executed
-// automatically with a newly created root-level router.
 
 // Register all the application routes. This is the main route registrer.
 func Register(router *goyave.Router) {
-
 	// Applying default CORS settings (allow all methods and all origins)
-	// Learn more about CORS options here: https://goyave.dev/guide/advanced/cors.html
 	router.CORS(cors.Default())
+	router.Middleware(ratelimiter.New(model.RateLimiterFunc))
 
-	// Register your routes here
+	authenticator := auth.Middleware(&model.User{}, &auth.JWTAuthenticator{})
+	UserRoutes(router, authenticator)
+}
 
-	// Route without validation
-	router.Get("/hello", hello.SayHi)
+func UserRoutes(parent *goyave.Router, authenticator goyave.Middleware) {
+	authRouter := parent.Subrouter("/auth")
 
-	// Route with validation
-	router.Post("/echo", hello.Echo).Validate(hello.EchoRequest)
+	jwtController := auth.NewJWTController(&model.User{})
+	jwtController.UsernameField = "email"
+	authRouter.Post("/register", user.Store).Validate(user.Structure)
+	authRouter.Post("/login", jwtController.Login).Validate(user.Structure)
+
+	userRouter := parent.Subrouter("/users")
+	userRouter.Middleware(authenticator)
+	userRouter.Get("/", user.Index)
+	userRouter.Get("/{id}", user.Show)
+	userRouter.Patch("/{id}", user.Update)
+	userRouter.Delete("/{id}", user.Destroy)
 }
